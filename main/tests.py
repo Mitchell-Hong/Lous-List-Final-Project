9abee168136a97ea3ad6536ffe7a07183d91fa76
Nathan <nts7bcj@virginia.edu>
2022-10-09 22:49:40 -0400

    making the test cases pass

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,8 @@ class URLTest(TestCase):
     def test_fake_url(self):
         response = self.client.get('/fake/')
         self.assertFalse(response.status_code == 200)
+
+from django.test import TestCase, override_settings
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class MyTest(TestCase):
+    pass
