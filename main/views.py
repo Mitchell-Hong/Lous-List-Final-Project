5d07ecad5893e9d5b13e2cf55621af335f42805c
Mitchell Hong <mch6eq@virginia.edu>
2022-10-17 21:26:57 -0400

    Added course catalog list

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from django.urls import reverse, path
 from .forms import UserForm
-from .models import myUser
+from .models import myUser, department
 # this is used for making HTTP requests from an external API with django
 import requests
 
@@ -15,13 +15,13 @@ def index(request):
 def editprofile(request):
     # request.user.id gives and id to each user, request.user will be the name of the user
     # https://docs.djangoproject.com/en/4.1/ref/contrib/auth/ info about user fields
-    
-    # try and get the model from the database if it exists then this user has signed in before 
+
+    # try and get the model from the database if it exists then this user has signed in before
     # do not make him fill out the form again just redirect to main
     try:
         newUser = myUser.objects.get(id=request.user.id)
-        return HttpResponseRedirect(reverse('main:index'))    
-    # if the user has not logged in yet then create a new user 
+        return HttpResponseRedirect(reverse('main:index'))
+    # if the user has not logged in yet then create a new user
     except:
         # users have id, name, email, summary, major, graduationYear
         newUser = myUser(id=request.user.id, name=str(request.user), summary='', major='', graduationYear='')
@@ -35,17 +35,17 @@ def editprofile(request):
                 form.save()
                 # reverse looks through all URLs defined in project and returns the one specified
                 # this is what we want so we have no hardcoded URLS
-                return HttpResponseRedirect(reverse('main:index'))     
+                return HttpResponseRedirect(reverse('main:index'))
         return render(request, 'main/editprofile.html', context)
 
 
 def coursecatalog(request):
     url = 'http://luthers-list.herokuapp.com/api/deptlist/'
     response = requests.get(url)
-    data = response.json()      # data is a list of departments {"subject": abbrev}
-    departments = []
+    data = response.json()
+    departments = department.objects.all() # data is a list of departments {"subject": abbrev}
+    context = { 'department_results' : departments }
     for i in data:
-        tempDepartment = i["subject"]
-        departments.append(tempDepartment)
-        
-    return HttpResponse(departments)
+        deps = department(abbreviation = i['subject'])
+        deps.save()
+    return render(request,'main/coursecatalog.html', context)
