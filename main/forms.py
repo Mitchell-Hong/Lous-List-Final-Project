3c06145cb63b7bd6110c63de1674be88ee3b4d82
Mitchell Hong <mch6eq@virginia.edu>
2022-11-02 16:24:32 -0400

    removed form from class search

--- main/forms.py
+++ main/forms.py
@@ -1,17 +1,9 @@
 from django.forms import ModelForm
 from django import forms
-from .models import myUser, department
+from .models import myUser
 
 class UserForm(ModelForm):
     class Meta:
         model = myUser
         fields = ['email', 'major', 'graduationYear', 'summary']
 
-class searchclassForm(forms.Form):
-    department_choices = department.objects.all()
-    into_text = []
-    for deps in department_choices:
-        into_text.append((deps.abbreviation, deps.abbreviation))
-    department = forms.CharField(label = "Select the department", widget=forms.Select(choices = into_text))
-    fields = ['department']
-    #class Meta:
