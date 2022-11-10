ad04d9aed3463d31e1d54da831b333bd3482973c
Edmund Chen <edchen7321@gmail.com>
2022-11-10 14:23:40 -0500

    Edmund- Homepage looks more stylized!

--- main/static/main/index/script.js
+++ main/static/main/index/script.js
@@ -22,3 +22,24 @@ function setDarkMode() {
         localStorage.removeItem('theme', 'night');
     }
 }
+
+ if (localStorage.getItem('theme') == 'dark') {
+            setDarkMode();
+
+            if (document.getElementById('moon').checked) {
+                localStorage.setItem('moon', true)
+            }
+        }
+
+        function setDarkMode() {
+            let isDark = document.body.classList.toggle('night');
+
+            if (isDark) {
+                setDarkMode.checked = true;
+                localStorage.setItem('theme' , 'night');
+                document.getElementById('moon').setAttribute('checked', 'checked');
+            } else {
+                setDarkMode.checked = true;
+                localStorage.removeItem('theme', 'night');
+            }
+        }
