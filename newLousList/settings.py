89069ffa4f00edb7934df4931f007708633a2821
Nathan <nts7bcj@virginia.edu>
2022-12-02 12:58:12 -0500

    stashing

--- newLousList/settings.py
+++ newLousList/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 # SHERRIFF: Added both the local host and herokuapp.com here to handled the DisallowedHost error.
 ALLOWED_HOSTS = ['localhost','127.0.0.1','louslist.herokuapp.com']
 
-SECURE_SSL_HOST = 'https://louslist.herokuapp.com/'
-SECURE_SSL_REDIRECT = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,9 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap5',
     'main',
-    'django.contrib.sitemaps',
-    'django.contrib.redirects',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
