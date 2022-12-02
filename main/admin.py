75a0dbc7f49e1fa8a4f31ac893ce6591708681b4
Nathan <nts7bcj@virginia.edu>
2022-12-02 10:43:07 -0500

    can see who each user friends / in admin, fixed numFriends in admin

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import myUser, course, department, Friend_Request, FriendList, ShoppingCart, ClassSchedule, Comment
 # Register your models here.
 
-admin.site.register(myUser)
+class FriendListInline(admin.TabularInline):
+    model = FriendList
+    extra = 5
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [FriendListInline]
+
+
+admin.site.register(myUser, UserAdmin)
 admin.site.register(course)
 admin.site.register(department)
 admin.site.register(Friend_Request)
